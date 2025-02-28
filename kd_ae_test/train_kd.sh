1b6b4fd23a6e1ed8a9f64e07be2942562f04108f
FABallemand <allemand.fabien@orange.fr>
2025-02-28 16:02:03 +0100

    Train StudentAE for image reconstruction

--- kd_ae_test/train_kd.sh
+++ kd_ae_test/train_kd.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --output=train_res/%j/%j_train_kd.out
 #SBATCH --error=train_res/%j/%j_train_kd.err
-#SBATCH --time=24:00:00
+#SBATCH --time=99:00:00
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=4
@@ -15,11 +15,11 @@ mkdir train_res/$SLURM_JOB_ID
 DATASET=/home/ids/fallemand-24/PRIM/data/vimeo/vimeo_triplet
 
 # TEACHER_CHECKPOINT=/home/ids/fallemand-24/PRIM/kd_ae/train_res/243807/checkpoint_best.pth.tar
-TEACHER_CHECKPOINT=/home/ids/fallemand-24/PRIM/kd_ae/train_res/254462/checkpoint_best.pth.tar
+TEACHER_CHECKPOINT=/home/ids/fallemand-24/PRIM/kd_ae/train_res/274518/checkpoint_best.pth.tar
 
 eval "$(conda shell.bash hook)"
 
 conda activate prim_env
 
 set -x
-srun python3 -u train_kd.py -d $DATASET --num-workers 2 --epochs 46 --batch-size 8 -lr 1e-4 --cuda --savepath train_res/$SLURM_JOB_ID --teacher-checkpoint $TEACHER_CHECKPOINT
+srun python3 -u train_kd.py -d $DATASET --num-workers 2 --epochs 1000 --batch-size 16 -lr 1e-4 --cuda --savepath train_res/$SLURM_JOB_ID --teacher-checkpoint $TEACHER_CHECKPOINT
